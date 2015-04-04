0cbce57d25a6cf1ee39ea057d46e84ac0e391e6c
dilgerm <martin@effectivetrainings.de>
2015-04-04 08:11:12 +0200

    initial ts setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
+        ts: {
+            default: {
+                src: ["src/script/**/*.ts"]
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-ts');
     grunt.registerTask('default', []);
 }
