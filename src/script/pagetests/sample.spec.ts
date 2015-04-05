84a5a3215863e2604c68545b64c88ee8cbeeea9e
dilgerm <martin@effectivetrainings.de>
2015-04-05 22:22:21 +0200

    enhance test operations

--- src/script/pagetests/sample.spec.ts
+++ src/script/pagetests/sample.spec.ts
@@ -2,13 +2,18 @@
 /// <reference path="../../reference.ts"/> ///ts:ref:generated
 module protractor.testing {
 
+    enum OperationType {
+        ADDITION, DIVISION, MODULO, MULTIPLICATION, SUBTRACTION
+    }
+
     class CalculatorPage {
 
         open() {
             browser.get("http://juliemr.github.io/protractor-demo/");
         }
 
-        fill(first:number, second:number) {
+        fill(operation:OperationType, first:number, second:number) {
+            element(by.model("operator")).$('[value="' + OperationType[operation] + '"]').click();
             element(by.model("first")).sendKeys(first.toString());
             element(by.model("second")).sendKeys(second.toString());
         }
@@ -20,7 +25,7 @@ module protractor.testing {
         verifyResult(expected:number) {
             var resultList = this.findResultList();
             //expect result to be correct
-            expect(resultList.first().element(by.binding("result.value")).getText()).toEqual(expected.toString);
+            expect(resultList.first().element(by.binding("result.value")).getText()).toEqual(expected.toString());
         }
 
         verifyNumberOfResults(numberOfResults:number) {
@@ -45,9 +50,37 @@ module protractor.testing {
         beforeEach(calculatorPage.open);
 
         it("it should add 2 operands", () => {
-            calculatorPage.fill(1, 2);
+            calculatorPage.fill(OperationType.ADDITION, 1, 2);
             calculatorPage.submit();
             calculatorPage.verifyResult(3);
+            calculatorPage.verifyNumberOfResults(1);
+        });
+
+        it("it should subtract 2 operands", () => {
+            calculatorPage.fill(OperationType.SUBTRACTION, 2, 2);
+            calculatorPage.submit();
+            calculatorPage.verifyResult(0);
+            calculatorPage.verifyNumberOfResults(1);
+        });
+
+        it("it should multiply 2 operands", () => {
+            calculatorPage.fill(OperationType.MULTIPLICATION, 2, 2);
+            calculatorPage.submit();
+            calculatorPage.verifyResult(4);
+            calculatorPage.verifyNumberOfResults(1);
+        });
+
+        it("it should divide 2 operands", () => {
+            calculatorPage.fill(OperationType.DIVISION, 20, 10);
+            calculatorPage.submit();
+            calculatorPage.verifyResult(2);
+            calculatorPage.verifyNumberOfResults(1);
+        });
+
+        it("it should modulo 2 operands", () => {
+            calculatorPage.fill(OperationType.MODULO, 5, 4);
+            calculatorPage.submit();
+            calculatorPage.verifyResult(1);
             calculatorPage.verifyNumberOfResults(1);
         });
     });
