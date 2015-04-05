b477cb8ef3866a0c7297244131c23af317326d47
dilgerm <martin@effectivetrainings.de>
2015-04-05 22:04:59 +0200

    refactor to page objects

--- src/script/pagetests/sample.spec.ts
+++ src/script/pagetests/sample.spec.ts
@@ -2,33 +2,53 @@
 /// <reference path="../../reference.ts"/> ///ts:ref:generated
 module protractor.testing {
 
-    export class Test {
+    class CalculatorPage {
 
-        public Test() {
+        open() {
+            browser.get("http://juliemr.github.io/protractor-demo/");
         }
 
-        protractorExample() {
-            //open page
-            browser.get("http://juliemr.github.io/protractor-demo/");
-            //fill form
-            element(by.model("first")).sendKeys("1");
-            element(by.model("second")).sendKeys("2");
-            element(by.id("gobutton")).click();
+        fill(first:number, second:number) {
+            element(by.model("first")).sendKeys(first.toString());
+            element(by.model("second")).sendKeys(second.toString());
+        }
 
+        submit() {
+            element(by.id("gobutton")).click();
+        }
+
+        verifyResult(expected:number) {
+            var resultList = this.findResultList();
+            //expect result to be correct
+            expect(resultList.first().element(by.binding("result.value")).getText()).toEqual(expected.toString);
+        }
+
+        verifyNumberOfResults(numberOfResults:number) {
             var resultList = element.all(by.repeater("result in memory"));
             //expect 1 result
-            expect(resultList.count()).toBe(1);
-            //expect result to be correct
-            expect(resultList.first().element(by.binding("result.value")).getText()).toBe('3');
+            expect(resultList.count()).toBe(numberOfResults);
         }
+
+        private findResultList() {
+            return element.all(by.repeater("result in memory"));
+        }
+
     }
 
     /**
      * simple jasmine spec
      */
-    describe("protractor test", () => {
-        it("should open angular js page", () => {
-            new Test().protractorExample();
+    describe("Calculator", () => {
+
+        var calculatorPage = new CalculatorPage();
+
+        beforeEach(calculatorPage.open);
+
+        it("it should add 2 operands", () => {
+            calculatorPage.fill(1, 2);
+            calculatorPage.submit();
+            calculatorPage.verifyResult(3);
+            calculatorPage.verifyNumberOfResults(1);
         });
     });
 }
